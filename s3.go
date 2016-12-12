54f8f3d747b95b847d6c645d7b87735199ae984d
leyaxproject <hsantos@leya.com>
2016-12-12 17:51:04 +0000

    Update and rename s2.go to s3.go

--- s3.go
+++ s3.go
@@ -1,7 +1,7 @@
 package xstringutils
 
 func FullName(f, l string) (string, int) {
-	full := f + " " + l + " from github do marco package xstringutils UPDATE 4."
+	full := f + " " + l + " from github do marco package xstringutils UPDATE 5."
 	length := len(full)
 
 	return full, length
