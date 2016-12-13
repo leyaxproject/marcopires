849c8530a186f06f2a754247b4a3357cd93fe26a
leyaxproject <hsantos@leya.com>
2016-12-13 15:27:42 +0000

    Update s3.go

--- s3.go
+++ s3.go
@@ -1,7 +1,7 @@
 package xstringutils
 
 func FullName(f, l string) (string, int) {
-	full := f + " " + l + " from github do marco package xstringutils UPDATE 5."
+	full := f + " " + l + " from github do marco package xstringutils UPDATE6."
 	length := len(full)
 
 	return full, length
